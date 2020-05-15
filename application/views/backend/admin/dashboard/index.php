172b7888995bb8ef328f000273a21b55e4b6060e
naufalipp <naufal211@gmail.com>
2020-05-15 16:18:13 +0700

    nyoba nambah welcome dashboard

--- application/views/backend/admin/dashboard/index.php
+++ application/views/backend/admin/dashboard/index.php
@@ -5,6 +5,7 @@
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800" id="page-title">Dashboard</h1>
     </div>
+    <div class="d-sm-flex align-items-center justify-content-between mb-4"><p>Selamat Datang</p></div>
 
     <!-- Content Row -->
     <div class="row">

7560334f964352d1d5be84f40615a971ea7b7f4b
naufalipp <naufal211@gmail.com>
2020-05-22 22:19:15 +0700

    profile-cobadikit

--- application/views/backend/mahasiswa/profile/index.php
+++ application/views/backend/mahasiswa/profile/index.php
@@ -6,5 +6,35 @@
         <h1 class="h3 mb-0 text-gray-800" id="page-title">Profile</h1>
     </div>
 
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Selamat datang ...</h6>
+        </div>
+        <div class="col-md-8">
+            <div class="form-group">
+                <div class="col-md-4 pr-0">
+                    <img src="" id="foto_profile" class="img-fluid">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-3 control-label text-right">Nama Mahasiswa <span class="text-danger">*</span>
+                </label>
+                <div class="col-sm-9">
+    				<input type="text" class="form-control" id="nama_ubah" name="nama" autocomplete="off">
+                        <p id="nama_ubah_error"></p>
+			     </div>
+            </div>
+
+            <div class="form-group row">
+                <label class="col-sm-3  control-label text-right">Universitas <span class="text-danger">*</span>
+                </label>
+                <div class="col-sm-9">
+    				<input type="text" class="form-control" id="nama_ubah" name="nama" autocomplete="off">
+                        <p id="nama_ubah_error"></p>
+			</div>
+            </div>
+
+
+    </div>
 </div>
 <!-- /.container-fluid -->

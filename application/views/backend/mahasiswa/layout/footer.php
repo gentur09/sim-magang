89ebda5d3272c42e5113e7a18fc72b293af9c810
Gentur Waskita <gentur020999@gmail.com>
2020-05-29 15:02:56 +0700

    Footer text

--- application/views/backend/mahasiswa/layout/footer.php
+++ application/views/backend/mahasiswa/layout/footer.php
@@ -5,7 +5,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; SIM Magang <?= date("Y"); ?></span>
+            <span>&copy; <?= date("Y"); ?>, SIM Magang</span>
         </div>
     </div>
 </footer>

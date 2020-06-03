0f59972c19dab0cfbeb38a9e589278bc72e2fde1
naufalipp <naufal211@gmail.com>
2020-06-03 12:23:38 +0700

    nambah sedikit profile  dan halaman Kinerja Mhs

--- application/controllers/backend/mahasiswa/Kinerja.php
+++ application/controllers/backend/mahasiswa/Kinerja.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Profile extends CI_Controller
+class Kinerja extends CI_Controller
 {
     public function __construct()
     {
